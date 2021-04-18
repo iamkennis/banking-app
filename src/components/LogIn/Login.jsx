4a3bde696c66e0faf8a8a662cd671186dafebd77
Amodu Kehinde <kennisdamilare@gmail.com>
2021-04-18 16:40:22 +0100

    still working on it

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState} from 'react'
 import accounts from '../../Data'
 // import { Route, Redirect } from 'react-router-dom';
 import Home from '../../pages/Home/Home'
@@ -9,27 +9,44 @@ export default function Login() {
 	// const classes = useStyles();
 	const [nameUser, setNameUser] = useState('');
 	const [password, setPassword] = useState('');
-	const [displayUser, setDisplayUser] = useState([])
+	const [displayUser, setDisplayUser] = useState(accounts)
 	
-	const createUser = (accounts) => {
-		accounts.forEach(account => {
-// Loop through the accounts array to return the elements
-			account.username = account.owner
-// transfer the account.owner to account.username, and change the usename to lowercase, split them, map them to retrun the first letter and join the first letter to form e.g "stw"
-				.toLowerCase()
-				.split(' ')
-				.map((name) => name[0])
-				.join('');
-			//   console.log(account.username);
-		});
-	}
-	createUser(accounts)
+const createUser = (accounts) => {
+	accounts.forEach((account) => {
+		// Loop through the accounts array to return the elements
+		account.username = account.owner
+			// transfer the account.owner to account.username, and change the usename to lowercase, split them, map them to retrun the first letter and join the first letter to form e.g "stw"
+			.toLowerCase()
+			.split(' ')
+			.map((name) => name[0])
+			.join('');
+		//   console.log(account.username);
+	});
+};
+createUser(accounts);
 
 
+	const handleBtn = (e) => {
+		let createAccount;
+		// declare a variable createAccount and pass the accounts.find has value to return and find the account.username if it equal the nameUser input into the form field.
+		createAccount = accounts.find(
+			(account) => account.username === nameUser
+		);
+		if (createAccount?.pin === Number(password)) {
+			return <Dashboard />;
+			// the if statement check for account.pin from the DATA.JS if its equal to the password input into the form field
+		}
+		setNameUser('');
+		setPassword('');
 
+		return createAccount;
+	};
+
+
+	
+	
 
 	const handleSubmit = (e) => {
-
 // To prevent the form from submitting
 		e.preventDefault();		
 	}
@@ -37,22 +54,6 @@ export default function Login() {
 	
 
 	
-	const handleBtn = (e) => {
-		
-		let createAccount;
-		// declare a variable createAccount and pass the accounts.find has value to return and find the account.username if it equal the nameUser input into the form field.
-	setDisplayUser(createAccount = accounts.find(
-			(account) => account.username === nameUser))
-		if (createAccount?.pin === Number(password)) {
-				console.log(setDisplayUser);
-			// the if statement check for account.pin from the DATA.JS if its equal to the password input into the form field
-			
-		}
-			setNameUser('');
-		setPassword('');
-		
-		return createAccount;
-	}
 
 // 		useEffect(() => {
 // 			if (createAccount) {
@@ -78,7 +79,7 @@ export default function Login() {
 					onChange={(e) => setPassword(e.target.value)}
 					value={password}
 				/>
-				<button className='login__btn ' onClick={handleBtn}>
+				<button className='login__btn ' onClick={() => setDisplayUser(...displayUser, handleBtn())}>
 					&rarr;
 				</button>
 			</form>
